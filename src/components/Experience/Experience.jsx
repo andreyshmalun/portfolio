9df25867299c44f97976765339f5967b123479fe
Andrey <shmalunandrey@gmail.com>
2022-12-12 17:47:43 -0500

    updated experience component

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,80 +1,99 @@
 import React from 'react';
-import { AiFillCheckCircle } from 'react-icons/ai';
 import './Experience.css';
 const Experience = () => {
   return (
     <section id="experience">
       <h5>What skills I have</h5>
       <h2>My experience</h2>
-
       <div className="container experience__container">
-        <div className="experience__frontend">
-          <h3>Frontend development</h3>
-          <div className="experience__content">
-            <article className="experience__details">
-              <AiFillCheckCircle className="experience__details-icon" />
-              <div>
-                <h4>HTML</h4>
-                <small>Experienced</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <AiFillCheckCircle className="experience__details-icon" />
-              <div>
-                <h4>CSS</h4>
-                <small>Experienced</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <AiFillCheckCircle className="experience__details-icon" />
-              <div>
-                <h4>Javascript</h4>
-                <small>Intermediate</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <AiFillCheckCircle className="experience__details-icon" />
-              <div>
-                <h4>React</h4>
-                <small>Intermediate</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <AiFillCheckCircle className="experience__details-icon" />
-              <div>
-                <h4>Bootstrap</h4>
-                <small>Intermediate</small>
-              </div>
-            </article>
+        <article className="experience__details">
+          <div>
+            <h3 className="experience__header">Languages</h3>
+            <div className="experience__list">
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Javascript</li>
+              <li>Typescript</li>
+            </div>
           </div>
-        </div>
-
-        <div className="experience__backend">
-          <h3>Backend development</h3>
-          <div className="experience__content">
-            <article className="experience__details">
-              <AiFillCheckCircle className="experience__details-icon" />
-              <div>
-                <h4>Node JS</h4>
-                <small>Intermediate</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <AiFillCheckCircle className="experience__details-icon" />
-              <div>
-                <h4>MongoDB</h4>
-                <small>Intermediate</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <AiFillCheckCircle className="experience__details-icon" />
-              <div>
-                <h4>REST</h4>
-                <small>Intermediate</small>
-              </div>
-            </article>
+        </article>
+        <article className="experience__details">
+          <div>
+            <h3 className="experience__header">
+              Frontend libraries & frameworks
+            </h3>
+            <div className="experience__list">
+              <li>jQuery</li>
+              <li>Bootstrap</li>
+              <li>React</li>
+              <li>React-Redux</li>
+              <li>Angular</li>
+              <li>Angular-Material</li>
+            </div>
           </div>
-        </div>
+        </article>
+        <article className="experience__details">
+          <div>
+            <h3 className="experience__header">Backend and other libraries</h3>
+            <div className="experience__list">
+              <li>NodeJS</li>
+              <li>Express</li>
+              <li>Morgan</li>
+              <li>CORS</li>
+              <li>Axios</li>
+              <li>Parcel</li>
+              <li>PropTypes</li>
+              <li>Serverless</li>
+              <li>Recharts</li>
+            </div>
+          </div>
+        </article>
+        <article className="experience__details">
+          <div>
+            <h3 className="experience__header">Testing & QA</h3>
+            <div className="experience__list">
+              <li>Postman</li>
+              <li>Jest</li>
+              <li>Enzyme</li>
+              <li>Cucumber</li>
+              <li>Puppeteer</li>
+            </div>
+          </div>
+        </article>
+        <article className="experience__details">
+          <div>
+            <h3 className="experience__header">Documentation</h3>
+            <div className="experience__list">
+              <li>JSDoc</li>
+              <li>TypeDoc</li>
+            </div>
+          </div>
+        </article>
+        <article className="experience__details">
+          <div>
+            <h3 className="experience__header">Web services & databases</h3>
+            <div className="experience__list">
+              <li>GitHub</li>
+              <li>AWS</li>
+              <li>MongoDB</li>
+              <li>PostgreSQL</li>
+              <li>Google Firestore</li>
+              <li>Heroku</li>
+              <li>Netlify</li>
+            </div>
+          </div>
+        </article>
+        <article className="experience__details">
+          <div>
+            <h3 className="experience__header">Mobile Apps</h3>
+            <div className="experience__list">
+              <li>PWA</li>
+              <li>React Native</li>
+              <li>Android Studio</li>
+              <li>Expo</li>
+            </div>
+          </div>
+        </article>
       </div>
     </section>
   );
